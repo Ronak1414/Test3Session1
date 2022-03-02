5d0c8ba85b8b4069e6c27db1d7a5b9a8e622340d
Ronak <96277743+Ronak1414@users.noreply.github.com>
2022-03-02 14:42:19 +0000

    p5 done!!

---
+++ p5original.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int size()
+{
+  int n;
+  printf("enter the array size:");
+  scanf("%d",&n);
+  return n;
+}
+
+void input(int n,int a[n])
+{
+  printf("enter the numbers :\n");
+  for(int i=0;i<n;i++)
+    {
+      scanf("%d",&a[i]);
+    }
+}
+int largest(int n,int a[n])
+{
+  int large;
+  large=a[0];
+  for(int i=1;i<n;i++)
+    {
+      if(a[i]>large)
+        large=a[i];
+    }
+  return large;
+}
+
+void output(int n,int a[n],int large)
+{ printf("The numbers are\n");
+  for(int i=0;i<n;i++)
+  {
+    printf(" %d",a[i]);
+  }
+  printf("\nThe largest among the above numbers is %d",large);
+}
+int main()
+{
+  int n,large;
+  n=size();
+  int a[n];
+  input(n,a);
+  large=largest(n,a);
+  output(n,a,large);
+  return 0;
+}
